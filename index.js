2a00a4adbd22805e76a833579a65e91b7af061f7
shakib04 <mdshakibulalam@gmail.com>
2022-08-23 12:43:23 +0600

    2 files modified

--- index.js
+++
@@ -1,7 +0,0 @@
-generateUATTagName();
-
-function generateUATTagName(){
-  uatTagName =  new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getUTCDate() + "-" + new Date().getHours() + new Date().getMinutes();
-
-  document.getElementById("uatTagName").innerText = uatTagName;
-}
